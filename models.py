b8cb56258954ea0f94d8f443a62e19c5a2719ecd
maxhirtens <max@maxhirtenstein.com>
2022-10-20 12:45:29 -0700

    init

---
+++ models.py
@@ -0,0 +1,87 @@
+"""Models for Blogly."""
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+  '''User class'''
+
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+  first_name = db.Column(db.String(20),
+                     nullable=False
+                     )
+
+  last_name = db.Column(db.String(30),
+                     nullable=False
+                     )
+
+  image_url = db.Column(db.String(200),
+                     nullable=True,
+                     default = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'
+                     )
+
+  posts = db.relationship("Post", backref="user")
+
+  def greet(self):
+        """Greet using name."""
+
+        return f"Thanks for logging in, {self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+  '''User posts'''
+
+  __tablename__ = 'posts'
+
+  id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+  title = db.Column(db.String(50),
+                     nullable=False
+                     )
+
+  content = db.Column(db.String(200),
+                     nullable=False
+                     )
+
+  created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Tag(db.Model):
+  '''Tags database'''
+
+  __tablename__ = 'tags'
+
+  id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+  name = db.Column(db.Text,
+                    unique=True)
+
+  posts = db.relationship('Post', backref='tags')
+
+class PostTag(db.Model):
+      '''Tags that go on posts'''
+
+      __tablename__ = 'post_tags'
+
+      post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+      tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
---
+++ seed.py
@@ -0,0 +1,35 @@
+'''Put sample data into our db.'''
+from app import app
+from models import User, Post, Tag, PostTag, db
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# If table isn't empty, empty it
+User.query.delete()
+
+# Add users
+max = User(first_name='Max', last_name='Hirtenstein')
+tom = User(first_name='Tom', last_name='Braider')
+linda = User(first_name='Linda', last_name='Wall')
+
+# Add test posts
+p1 = Post(title='Test 1', content="Test 1 Content", user_id='3')
+p2 = Post(title='Test 2', content="Test 2 Content", user_id='3')
+p3 = Post(title='Test 3', content="Test 3 Content", user_id='1')
+
+# Add test tags
+t1 = Tag(name='funny')
+t2 = Tag(name='cool')
+
+# Add tags to posts
+pt1 = PostTag(post_id=1, tag_id=2)
+pt2 = PostTag(post_id=2, tag_id=1)
+
+# Add new objects to session, so they'll persist
+db.session.add_all([max, tom, linda])
+db.session.add_all([p1, p2, p3])
+
+# Commit--otherwise, this never gets saved!
+db.session.commit()
